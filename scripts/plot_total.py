d4a65f4f7ea8f61eeb0e9a2f0f532e631e0f1ba7
nick-jhlee <jh_lee00@kaist.ac.kr>
2024-08-07 14:57:31 +0900

    Implement plot_total.py, Update README

--- scripts/plot_total.py
+++ scripts/plot_total.py
@@ -16,6 +16,8 @@ optional arguments:
 
 import argparse
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+
 import numpy as np
 import os
 import json
@@ -30,7 +32,7 @@ parser.add_argument('-d', type=int, nargs='?', default=2, help='Dimension')
 parser.add_argument('-hz', type=int, nargs='?', default=4002, help='Horizon length)')
 parser.add_argument('-ast', type=str, nargs='?', default='tv_discrete',
                     help='Arm set type. Must be either fixed_discrete, tv_discrete or ball')
-parser.add_argument('-Nconfidence', type=int, nargs='?', default=1000,
+parser.add_argument('-Nconfidence', type=int, nargs='?', default=2000,
                     help='Number of discretizations (per axis) for confidence set plot')
 
 args = parser.parse_args()
@@ -43,16 +45,27 @@ print(r"Plotting regret curves and confidence sets for $d=${}, $H=${} and arm_se
 
 alg_dict = {"OFUGLB": "OFUGLB", "OFUGLB-e": "OFUGLB-e", "EMK": "EMK", "RS-GLinCB": "RS-GLinCB", "OFULogPlus": "OFULog+",
             "adaECOLog": "ada-OFU-ECOLog"}
-alphas = [1, 1, 0.4, 0.4, 0.4, 0.4]
-colors = ['red', 'orange', 'blue', 'black', 'green', 'purple']
+color_dict = {"OFUGLB": "red", "OFUGLB-e": "orange", "EMK": "blue", "RS-GLinCB": "black", "OFULogPlus": "green",
+              "adaECOLog": "purple"}
+alpha_dict = {"OFUGLB": 1, "OFUGLB-e": 1, "EMK": 0.4, "RS-GLinCB": 0.4, "OFULogPlus": 0.4, "adaECOLog": 0.4}
 clrs = sns.color_palette("husl", 4)
 
-fig, axes = plt.subplots(3, 4, figsize=(30, 15))
+fig, axes = plt.subplots(3, 4, figsize=(30, 20))
 plt.rcParams.update({
     # "text.usetex": True,
     # "font.family": "sans-serif",
     # "font.sans-serif": ["Helvetica"],
-    "font.size": 17})
+    "font.size": 24})
+tick_font_size = 24
+
+cols = [r"$S=4$", r"$S=6$", r"$S=8$", r"$S=10$"]
+rows = ['Regret plots', 'Magnified regret plots', 'Confidence sets']
+
+for ax, col in zip(axes[0], cols):
+    ax.set_title(col)
+
+for ax, row in zip(axes[:, 0], rows):
+    ax.set_ylabel(row, size='large')
 
 lines, labels = [], []
 for row in range(3):
@@ -98,28 +111,41 @@ for row in range(3):
             # plotting
             with sns.axes_style("whitegrid"):
                 tmp = None
-                for i, algorithm in enumerate(["OFUGLB", "OFUGLB-e", "RS-GLinCB", "OFULogPlus", "EMK", "adaECOLog"]):
+                for algorithm in ["OFUGLB", "OFUGLB-e", "EMK", "RS-GLinCB", "OFULogPlus", "adaECOLog"]:
                     alg_name = alg_dict[algorithm]
                     regret = res_dict_mean[algorithm]
                     std = res_dict_std[algorithm]
-                    tmp, = axes[row, plt_idx].plot(regret, label=alg_name, color=colors[i], alpha=alphas[i])
+                    tmp, = axes[row, plt_idx].plot(regret, label=alg_name, color=color_dict[algorithm], alpha=alpha_dict[algorithm])
                     axes[row, plt_idx].fill_between(range(len(regret)), regret - std, regret + std, alpha=0.3,
-                                                    color=colors[i])
+                                                    color=color_dict[algorithm])
                     if plt_idx == 0 and row == 0:
                         lines.append(tmp)
                         labels.append(tmp.get_label())
-            axes[row, plt_idx].tick_params(axis='both', which='major', labelsize=15)
-            axes[row, plt_idx].tick_params(axis='both', which='minor', labelsize=15)
+            axes[row, plt_idx].tick_params(axis='both', which='major', labelsize=tick_font_size)
+            axes[row, plt_idx].tick_params(axis='both', which='minor', labelsize=tick_font_size)
+
             ## hard coding y-axis limits
             if row == 0:
                 if S == 4:
                     axes[row, plt_idx].set_ylim([0, 160])
+                    rect = Rectangle((0, 0), 4050, 35, linestyle='dashed', edgecolor='black',
+                                     facecolor='None', clip_on=False, linewidth=2.0)
+                    axes[row, plt_idx].add_patch(rect)
                 elif S == 6:
                     axes[row, plt_idx].set_ylim([0, 200])
+                    rect = Rectangle((0, 0), 4050, 25, linestyle='dashed', edgecolor='black',
+                                     facecolor='None', clip_on=False, linewidth=2.0)
+                    axes[row, plt_idx].add_patch(rect)
                 elif S == 8:
                     axes[row, plt_idx].set_ylim([0, 500])
+                    rect = Rectangle((0, 0), 4050, 20, linestyle='dashed', edgecolor='black',
+                                     facecolor='None', clip_on=False, linewidth=2.0)
+                    axes[row, plt_idx].add_patch(rect)
                 elif S == 10:
                     axes[row, plt_idx].set_ylim([0, 1000])
+                    rect = Rectangle((0, 0), 4050, 15, linestyle='dashed', edgecolor='black',
+                                     facecolor='None', clip_on=False, linewidth=2.0)
+                    axes[row, plt_idx].add_patch(rect)
                 else:
                     pass
             else:
@@ -141,58 +167,62 @@ for row in range(3):
             x, y = np.meshgrid(interact_rng, interact_rng)
 
             fnames = ["OFUGLB.npz", "OFUGLB-e.npz", "EMK.npz", "OFULogPlus.npz"]
-            alg_names = ["OFUGLB", "OFUGLB-e", "EMK", "OFULog+"]
-            colors = ['red', 'orange', 'blue', 'green']
-
-            tick_font_size = 24
-            # if S == 5:
-            #     displacements = [(-0.2, 0.0), (-0.1, -0.35), (0.0, 0.0)]
-            #     manual_locations = [[(2.0, 1.0)], [(2.0, -2.0)]]   # label location for contourf
-            # elif S == 10:
-            #     displacements = [(0.0, 0), (-0.4, 0.0), (0.0, -0.2)]
-            #     manual_locations = [[(6.0, 0.0)], [(6.0, 0.0)]]   # label location for contourf
-            # else:
-            #     print(f"For better plots, manually set displacements and contourf label locations for S={S}")
-            displacements = [(0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0)]
-            manual_locations = [[(0.0, 0.0)], [(0.0, 0.0)], [(0.0, 0.0)], [(0.0, 0.0)]]  # label location for contourf
-
-            # plotting
-            plt.figure(1, figsize=(12, 12))
+            # # label location for contourf
+            # displacements = [(0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0), (0.0, 0.0)]
+            # manual_locations = [[[(0.0, 0.0)], [(0.0, 0.0)], [(0.0, 0.0)], [(0.0, 0.0)]],
+            #                     [[(0.0, 0.0)], [(0.0, 0.0)], [(0.0, 0.0)], [(0.0, 0.0)]],
+            #                     [[(0.0, 0.0)], [(0.0, 0.0)], [(0.0, 0.0)], [(0.0, 0.0)]],
+            #                     [[(0.0, 0.0)], [(0.0, 0.0)], [(0.0, 0.0)], [(0.0, 0.0)]]]
             # with sns.axes_style("whitegrid"):
-            for i, fname in enumerate(fnames):
+            i = 0
+            for fname in fnames:
+                alg_name = fname.split(".")[0]
                 fname = f"S={S}/{arm_set_type}/{fname}"
                 with np.load(fname) as data:
                     z = data['z']
                     # plt.imshow(z, extent=(x.min(), x.max(), y.min(), y.max()), origin="lower", cmap="Greys", alpha=0.5)
-                    CS = plt.contour(x, y, z, levels=[0, 1], colors=colors[i])
-                    axes[row, plt_idx].clabel(CS, CS.levels[::2], inline=True, fmt=alg_names[i], fontsize=40,
-                                              manual=manual_locations[i])
-                    axes[row, plt_idx].contourf(x, y, z, [1 - 1e-12, 1 + 1e-12], colors=colors[i], alpha=0.1 + i * 0.05)
+                    CS = axes[row, plt_idx].contour(x, y, z, levels=[0, 1], colors=color_dict[alg_name])
+                    # axes[row, plt_idx].clabel(CS, CS.levels[::2], inline=True, fmt=alg_names[i], fontsize=40,
+                    #                           manual=manual_locations[plt_idx][i])
+                    axes[row, plt_idx].contourf(x, y, z, [1 - 1e-12, 1 + 1e-12], colors=color_dict[alg_name],
+                                                alpha=0.1 + i * 0.05)
 
                     theta_hat = data['theta_hat']
-                    # print(np.linalg.norm(theta_hat))
-                    axes[row, plt_idx].scatter(theta_hat[0], theta_hat[1], color=colors[i])
-                    axes[row, plt_idx].annotate(r" $\widehat{\theta}$", theta_hat + displacements[i], color=colors[i],
-                                                fontsize=35)
-            axes[row, plt_idx].scatter(theta_star[0], theta_star[1], color="blue", marker='*', s=170)
-            axes[row, plt_idx].annotate(r" $\theta_{\star}$", theta_star + displacements[-1], color='blue', fontsize=35)
+                    i += 1
+                    # axes[row, plt_idx].scatter(theta_hat[0], theta_hat[1], color=colors[i])
+                    # axes[row, plt_idx].annotate(r" $\widehat{\theta}$", theta_hat + displacements[i], color=colors[i],
+                    #                             fontsize=30)
+            axes[row, plt_idx].scatter(theta_star[0], theta_star[1], color="black", marker='*', s=170)
+            axes[row, plt_idx].annotate(r" $\theta_{\star}$", theta_star, color='black', fontsize=20)
+            # axes[row, plt_idx].annotate(r" $\theta_{\star}$", theta_star + displacements[-1], color='black',
+            #                             fontsize=20)
 
             z_ = (np.linalg.norm(np.array([x, y]), axis=0) <= S).astype(int)
             axes[row, plt_idx].contourf(x, y, z_, levels=[1 - 1e-12, 1 + 1e-12], alpha=0.1)
 
-            # plt.xlim([-S - 0.5, S + 0.5])
-            # plt.ylim([-S - 0.5, S + 0.5])
-            # plt.xlim([0.5, S + 0.1])
-            # plt.ylim([0.5, S + 0.1])
+            if S == 4 or S == 6:
+                axes[row, plt_idx].set_xlim([0, S + 0.1])
+                axes[row, plt_idx].set_ylim([0, S + 0.1])
+            elif S == 8:
+                axes[row, plt_idx].set_xlim([-1, S + 0.1])
+                axes[row, plt_idx].set_ylim([-1, S + 0.1])
+            else:
+                axes[row, plt_idx].set_xlim([-5, S + 0.1])
+                axes[row, plt_idx].set_ylim([-5, S + 0.1])
+
             axes[row, plt_idx].tick_params(axis='both', which='major', labelsize=tick_font_size)
             axes[row, plt_idx].tick_params(axis='both', which='minor', labelsize=tick_font_size)
 
-fig.legend(lines, labels, loc='upper center', ncol=min(10, len(alg_dict)), prop={'size': 17},
-           bbox_to_anchor=(0.5, 1.065))
-# plt.xlabel(r"$T$")
-# plt.ylabel(r"$Reg^B(T)$")
+leg = fig.legend(lines, labels, loc='upper center', ncol=min(10, len(alg_dict)), prop={'size': 32},
+           bbox_to_anchor=(0.5, 1.062))
+
+# https://stackoverflow.com/questions/9706845/increase-the-linewidth-of-the-legend-lines-in-matplotlib
+# set the linewidth of each legend object
+for legobj in leg.legend_handles:
+    legobj.set_linewidth(5.0)
+
 fig.tight_layout()
 
 plt.savefig(f"H={H}.pdf", dpi=400, bbox_inches='tight')
 plt.savefig(f"H={H}.png", dpi=400, bbox_inches='tight')
-plt.show(bbox_inches='tight')
+plt.show()
